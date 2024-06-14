97ba86b79332a0f37eab9e67bdb100a993b5f7fb
teste <teste>
2024-06-14 17:16:58 -0300

    Adicionar arquivos de projeto.

---
+++ sla/BuscaCEP.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sla
+{
+    internal class BuscaCEP
+    {
+
+
+        public string Cep { get; set; }
+        public string Rua { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Ibge { get; set; }
+
+        public string Ddd { get; set; }
+
+        public string Gia { get; set; }
+        
+        public string Siafi { get; set; }
+
+
+
+        public BuscaCEP(int c)
+        {
+
+            int cep = c;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + cep + "/json/");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+
+            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show("Erro na requisição: " + ChecaServidor.StatusCode.ToString());
+                return; // Encerra o código
+            }
+
+            using (Stream webStream = ChecaServidor.GetResponseStream())
+            {
+                if (webStream != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(webStream))
+                    {
+                        String response = responseReader.ReadToEnd();
+                        //MessageBox.Show(response);
+                        response = Regex.Replace(response, "[{},]", string.Empty);
+                        response = response.Replace("\"", "");
+                        //MessageBox.Show(response);
+
+                        String[] substrings = response.Split('\n');
+
+                        int cont = 0;
+                        foreach (var substring in substrings)
+                        {
+                            // CEP
+                            if (cont == 1)
+                            {
+                                string[] valor = substring.Split(':');
+                                Cep = valor[1].ToString();
+                            }
+
+                            // Logradouro
+                            if (cont == 2)
+                            {
+                                string[] valor = substring.Split(':');
+                                Rua = valor[1].ToString();
+                            }
+
+                            // Bairro
+                            if (cont == 4)
+                            {
+                                string[] valor = substring.Split(':');
+                                Bairro = valor[1].ToString();
+                            }
+
+                            // Cidade
+                            if (cont == 5)
+                            {
+                                string[] valor = substring.Split(':');
+                                Cidade = valor[1].ToString();
+                            }
+
+                            // UF
+                            if (cont == 6)
+                            {
+                                string[] valor = substring.Split(':');
+                                Estado = valor[1].ToString();
+                            }
+
+                            // IBGE
+                            if (cont == 7)
+                            {
+                                string[] valor = substring.Split(':');
+                                Ibge = valor[1].ToString();
+                            }
+
+                            // DDD
+                            if (cont == 9)
+                            {
+                                string[] valor = substring.Split(':');
+                                Ddd = valor[1].ToString();
+                            }
+
+                            // GIA
+                            if (cont == 8)
+                            {
+                                string[] valor = substring.Split(':');
+                                Gia = valor[1].ToString();
+                            }
+
+                            // SIAFI
+                            if (cont == 10)
+                            {
+                                string[] valor = substring.Split(':');
+                                Siafi = valor[1].ToString();
+                            }
+
+                            cont++;
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ sla/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            //executaBuscaBasica();
+            //executaBusca();
+            executaBuscaTratada();
+          
+          
+
+           
+        }
+
+        private void executaBuscaBasica()
+        {
+            int c = Convert.ToInt32(msktCepPesquisar.Text);
+
+            BuscaCEP buscaCEP = new BuscaCEP(c);
+
+            txtLogradouro.Text = buscaCEP.Rua;
+            txtBairro.Text = buscaCEP.Bairro;
+            txtCidade.Text = buscaCEP.Cidade;  
+            txtUf.Text = buscaCEP.Estado;
+        }
+        private void executaBusca()
+        {
+            try
+            {
+
+                msktCepPesquisar.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+                var valorSemMascara1 = msktCepPesquisar.Text;
+
+                int c;
+                string entrada = valorSemMascara1;
+
+                if (int.TryParse(entrada, out c))
+                {
+                    BuscaCEP buscaCEP = new BuscaCEP(c);
+
+                    txtLogradouro.Text = buscaCEP.Rua;
+                    txtBairro.Text = buscaCEP.Bairro;
+                    txtCidade.Text = buscaCEP.Cidade;
+                    txtUf.Text = buscaCEP.Estado;
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, insira um CEP válido.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    msktCepPesquisar.Clear();
+                    msktCepPesquisar.Focus();
+                }
+                msktCepPesquisar.TextMaskFormat = MaskFormat.IncludePromptAndLiterals;
+            }
+            catch
+            {
+                MessageBox.Show("Por favor, insira um CEP válido.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                msktCepPesquisar.Clear();
+                msktCepPesquisar.Focus();
+            }
+
+            
+        }
+        private void executaBuscaTratada()
+        {
+            try
+            {
+
+                msktCepPesquisar.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+                var valorSemMascara1 = msktCepPesquisar.Text;
+
+                int c;
+                string input = valorSemMascara1;
+
+                if (int.TryParse(input, out c) && input.Length == 8)
+                {
+                    BuscaCEP buscaCEP = new BuscaCEP(c);
+
+                    if (string.IsNullOrEmpty(buscaCEP.Cep))
+                    {
+                        limparcampos();
+                        MessageBox.Show("CEP não encontrado." + "Verifique o número digitado e tente novamente." , "CEP Inválido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        msktCepPesquisar.Clear();
+                        msktCepPesquisar.Focus();
+                    }
+                    else
+                    {
+                        txtLogradouro.Text = buscaCEP.Rua;
+                        txtBairro.Text = buscaCEP.Bairro;
+                        txtCidade.Text = buscaCEP.Cidade;
+                        txtUf.Text = buscaCEP.Estado;
+                    }
+
+                   
+                }
+                msktCepPesquisar.TextMaskFormat = MaskFormat.IncludePromptAndLiterals;
+            }
+            catch
+            {
+                limparcampos();
+                MessageBox.Show("Por favor, insira um CEP válido.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                msktCepPesquisar.Focus();
+            }
+        }
+        private void limparcampos()
+        {
+            txtBairro.Clear();
+            txtCidade.Clear();
+            txtLogradouro.Clear();
+            txtUf.Clear();
+            msktCepPesquisar.Clear();
+        }
+
+        private void msktCepPesquisar_Leave(object sender, EventArgs e)
+        {
+            if (msktCepPesquisar.MaskCompleted)
+            {
+                executaBuscaTratada();
+            }
+            
+        }
+    }
+}
